d511dafae44848662422efc89edf409c11ac0424
unknown <marziodellabosca149@gmail.com>
2022-05-23 15:24:42 +0200

    staff extentions

---
+++ src/Shop/ShopStaff.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Shop;
+
+import administration.Handler;
+import administration.Staff;
+
+/**
+ *
+ * @author Mars_DB
+ */
+public class ShopStaff extends Staff{
+    
+    public ShopStaff(String name, String password, Handler h) {
+        super(name, password, h);
+    }
+    
+}
---
+++ src/restourant/RestourantStaff.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restourant;
+
+import administration.Handler;
+import administration.Staff;
+
+/**
+ *
+ * @author Mars_DB
+ */
+public class RestourantStaff extends Staff{
+    
+    public RestourantStaff(String name, String password, Handler h) {
+        super(name, password, h);
+    }
+    
+}
